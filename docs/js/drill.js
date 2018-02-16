0db6f1ac11e9341d65b51f8dd2f1045696d9406a
SorutoProject <32834893+SorutoProject@users.noreply.github.com>
2018-02-16 02:42:36 +0000

    Update drill.js

--- docs/js/drill.js
+++ docs/js/drill.js
@@ -1,1 +1,23 @@
-
+window.onload = function(){
+var xhr = new XMLHttpRequest();
+  xhr.open('GET', location.href, true);
+  xhr.onreadystatechange = function(){
+    if (xhr.readyState === 4 && xhr.status === 200){
+      so.setVal("qlist",xhr.responseText);
+    }
+    if (xhr.readyState === 4 && xhr.status === 0){
+      so.setVal("qlist",xhr.responseText);
+    }
+  };
+  xhr.send(null);
+  showQ();
+};
+function showQ(){
+var dlist = so.getVal("qlist").value.split("\r\n");
+var nq = dlist[0];
+var nqlist = nq.split("|");
+if(nqlist[0]=="c4"){
+so.divIH("question",nqlist[1]);
+so.setVal()
+}
+}
